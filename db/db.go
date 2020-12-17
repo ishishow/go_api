24f67b67363225bd9ab3080048a90e15bbba5178
ishishow <ishikawa.sho.infratop@gmail.com>
2020-12-17 20:29:23 +0900

    wip

--- db/db.go
+++ db/db.go
@@ -1,8 +1,11 @@
-package conf
+package db
 
 import (
+	"fmt"
 	"encoding/json"
 	"io/ioutil"
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
 )
 
 // DB設定の構造体
@@ -17,24 +20,39 @@ type ConfDB struct {
 
 // URL設定の構造体
 func ReadConfDB() (*ConfDB, error) {
-
 	// 設定ファイル名
-	const conffile = "conf/db.json"
-
+	const conffile = "db/db.json"
 	// 構造体を初期化
 	conf :=new(ConfDB)
-
 	// 設定ファイルを読み込む
 	cValue, err := ioutil.ReadFile(conffile)
 	if err != nil {
 		return conf, err
 	}
-
 	// 読み込んだjson文字列をデコードし、構造体にセット
 	err = json.Unmarshal([]byte(cValue), conf)
 	if err != nil {
 		return conf, err
 	}
-
 	return conf, nil
+}
+
+func ConnectDB()(*sql.DB, error) {
+	//設定ファイルを読み込む
+	confDB, err := ReadConfDB()
+	if err != nil {
+			fmt.Println(err.Error())
+	}
+	//設定から接続文字列を生成
+	conStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s", confDB.User, confDB.Pass, confDB.Host, confDB.Port, confDB.DbName, confDB.Charset)
+	// データベース接続
+	db, err := sql.Open("mysql", conStr)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println("ok")
+	// // deferで終了前に必ずクローズする
+	// defer db.Close()
+	return db, err
 }
